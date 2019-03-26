8abec43c92430e6e952be8fb5f7ed239f04b982f
Frank Bennett <biercenator@gmail.com>
2019-03-26 16:36:32 +0900

    Fix varname

--- getSchemaPaths.js
+++ getSchemaPaths.js
@@ -2,6 +2,6 @@ var path = require("path");
 var schemaPathsCsl = path.join(__dirname, "schema", "csl.rnc");
 var schemaPathsCslm = path.join(__dirname, "schema", "csl-mlz.rnc");
 module.exports = {
-    csl: schemaPathCsl,
-    cslm: schemaPathCslm
+    csl: schemaPathsCsl,
+    cslm: schemaPathsCslm
 };
